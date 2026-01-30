b2e3cd0a5d8ebfb15bd91c2e25212d69499aface
nichapat-GG <nichapat.farsai@gmail.com>
2026-01-30 17:30:25 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
+
+struct scammer_phone{
+    string phoneNum;
+    int reportN;
+};
 
 int main(){
 
